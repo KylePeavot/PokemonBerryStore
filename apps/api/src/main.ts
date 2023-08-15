c06917ec26cf34b4ee4797da7dc728f5abf7bcc2
kylepeavot <kyle.peavot@crimsontide.com>
2023-08-15 15:19:16 +0100

    feat: DTS-18526 Enabled cors requests in the backend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import routesRouter from './routes'
+import express from 'express';
+import cors from 'cors';
+import routesRouter from './routes';
 
 const app = express();
-
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.use(cors());
+app.listen(3000, () => console.log('Listening on port 3000'));
 
 app.use('/', routesRouter);
