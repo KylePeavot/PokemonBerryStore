5f0e74a33eaf54596df6a446213f5413263ec02b
kylepeavot <kyle.peavot@crimsontide.com>
2023-08-17 15:05:38 +0100

    refactor: DTS-18528 Moved Berry components into Nx lib

--- apps/berry-store/src/app/berries/berries.module.ts
+++ apps/berry-store/src/app/berries/berries.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { BerriesShellPage } from './berries-shell/berries-shell.page';
 
 import { BerriesPageRoutingModule } from './berries-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { berryReducer } from './state/berry.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { BerryEffects } from './state/berry.effects';
-import { BerryListComponent } from './berry-list/berry-list.component';
-import { BerryFiltersComponent } from './berry-filters/berry-filters.component';
+import {
+	BerriesShellPage,
+	BerryFiltersComponent,
+	BerryListComponent,
+} from '@pokemon-berry-store/mobile/berries/components';
 
 @NgModule({
 	imports: [
